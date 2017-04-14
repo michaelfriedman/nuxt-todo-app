11212c5b7e1c01441b2c1af411f5c66a534256a4
Michael Friedman <contact@michaeldavidfriedman.com>
2017-04-13 22:41:28 -0700

    Nuxt project setup. counter is rendering and functioning

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    hello World
+   {{ counter }}
+   <button @click="increment">+</button>
+   <button @click="decrement">-</button>
+
   </div>
 </template>
 
+<script>
+  import { mapState, mapMutations } from 'vuex';
+
+  export default {
+    computed: {
+      ...mapState({
+        counter: state => state.counter
+      })
+    },
+    methods: {
+      ...mapMutations([
+        'increment',
+        'decrement'
+      ])
+    }
+  };
+</script>
