c9c8fb02a48a9a8c767c375f9ef215aa1843a5b7
Michael Friedman <contact@michaeldavidfriedman.com>
2017-04-14 07:01:09 -0700

    add complete and delete functionality

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,18 @@
   <div>
     <form @submit.prevent="add(task)">
       <input v-model="task " type="text" name="task ">
-      <button>Submit</button>
+      <input type="submit" value="add" />
     </form>
     <article class="pa3 pa5-ns">
       <h1 class="f4 bold center mw6">Michael Friedman's Todo List <small>made with Vue / Nuxt</small></h1>
       <ul class="list pl0 ml0 center mw6 ba b--light-silver br2">
-        <li v-for="todo of todos" class="ph3 pv3 bb b--light-silver">{{ todo.task}}</li>
+        <li v-for="todo of todos" class="flex items-center ph3 pv3 bb b--light-silver">
+          <span v-bind:class="{ strike: todo.complete}" class="flex-auto">
+            {{ todo.id }}. {{ todo.task }}
+            <button @click="remove(todo)"><img src="https://icon.now.sh/trash"></button>
+            <button @click="toggle(todo)"><img src="https://icon.now.sh/check"></button>
+          </span>
+        </li>
       </ul>
     </article>
   </div>
@@ -39,7 +45,9 @@
     },
     methods: {
       ...mapActions([
-        'add'
+        'add',
+        'remove',
+        'toggle'
       ])
     }
   };
--- store/index.js
+++ store/index.js
@@ -11,12 +11,35 @@ export const mutations = {
   },
   add (state, todo) {
     state.todos = [...state.todos, todo];
+  },
+  remove (state, todo) {
+    state.todos = state.todos.filter(t => t.id !== todo.id);
+  },
+  toggle (state, todo) {
+    state.todos = state.todos.map(t =>
+      t.id === todo.id
+      ? todo
+      : t);
   }
 };
 
 export const actions = {
   async add ({ commit }, task) {
+    // eslint-disable-next-line
     const res = await axios.post('https://todos-fpqdaxkgqt.now.sh/todos', { task, complete: false });
-    commit('add');
+    commit('add', res.data);
+  },
+  async remove ({ commit }, todo) {
+        // eslint-disable-next-line
+    const res = await axios.delete(`https://todos-fpqdaxkgqt.now.sh/todos/${todo.id}`);
+    commit('remove', todo);
+  },
+  async toggle ({ commit }, todo) {
+        // eslint-disable-next-line
+    const res = await axios.patch(`https://todos-fpqdaxkgqt.now.sh/todos/${todo.id}`,
+      {
+        complete: !todo.complete
+      });
+    commit('toggle', res.data);
   }
 };
